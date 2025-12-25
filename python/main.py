e7a41747f2ec0478dfa134bca05f79000764b436
Luca Scheuer <scheuer.luca@gmail.com>
2025-12-25 14:51:39 -0600

    making main call animate properly

--- python/main.py
+++ python/main.py
@@ -1,10 +1,43 @@
 import subprocess
 from pathlib import Path
+from animate import find_data_animate
 
 sim_path = Path(__file__).resolve().parent.parent / "cpp" / "build" / "3d_aircraft_sim"
-out_folder = Path(__file__).resolve().parent.parent / "out"
+out_folder = Path(__file__).resolve().parent.parent / "out_files"
+param_folder = Path(__file__).resolve().parent.parent / "param_files"
+
+# output files
 state_out_file = out_folder / "states.csv"
 state_dot_out_file = out_folder / "states_dot.csv"
 control_out_file = out_folder / "control.csv"
+forces_out_file = out_folder / "forces.csv"
 
-subprocess.run([sim_path, state_out_file, state_dot_out_file, control_out_file])
+# input files
+aircraft_model_params_file = param_folder / "aircraft_model_params.toml"
+controller_params_file = param_folder / "controller_params.toml"
+run_params_file = param_folder / "run_settings.toml"
+initial_condition_params_file = param_folder / "initial_conditions.toml"
+trajectory_file = param_folder / "trajectory.csv"
+
+input_list = [
+    sim_path,
+    state_out_file,
+    state_dot_out_file,
+    control_out_file,
+    forces_out_file,
+    aircraft_model_params_file,
+    controller_params_file,
+    run_params_file,
+    initial_condition_params_file,
+    trajectory_file,
+]
+
+subprocess.run(input_list)
+
+find_data_animate(
+    state_out_file,
+    forces_out_file,
+    control_out_file,
+    aircraft_model_params_file,
+    run_params_file,
+)
