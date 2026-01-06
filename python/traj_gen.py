1d1f56a62876c46ff4b8272f58d8e1292c003a5d
Luca Scheuer <scheuer.luca@gmail.com>
2026-01-06 14:36:48 -0600

    updating traj gen

--- python/traj_gen.py
+++ python/traj_gen.py
@@ -10,23 +10,39 @@ traj_gen_path = (
     / "min_snap_generator"
 )
 out_folder = Path(__file__).resolve().parent.parent / "trajectory_files"
+waypoint_folder = Path(__file__).resolve().parent.parent / "waypoint_files"
 
 # output files
-trajectory_file = out_folder / "new_traj.csv"
+trajectory_file = out_folder / "trajectory.csv"
+waypoint_file = waypoint_folder / "waypoints.csv"
 
 controller_frequency = 1000
+traj_speed = 35
+speed_weight = 0
+descent_rate = 0.00000001
+step_limit = 1000
 
-waypoints = [
-    [0, 0, 0, 0],
-    [5, 0, 0, np.pi / 2],
-    [10, 0, 0, 0],
-]
+angle_step = np.pi / 8
+diameter = 16
+lateral_offset = 0.1
+angles = np.arange(0, np.pi * 2 + angle_step, angle_step)
+xs = 0.5 * diameter * np.cos(angles - np.pi / 2) + lateral_offset
+ys = 0.5 * diameter * np.sin(angles - np.pi / 2) + diameter * 0.5
+waypoints = [[0, 0, 0, 0]]
+for x, y, angle in zip(xs, ys, angles):
+    waypoints.append([x, y, 0, angle])
+waypoints.append([lateral_offset + lateral_offset, 0, 0, np.pi * 4.0])
+
 
 input_list = [
     traj_gen_path,
     trajectory_file,
+    waypoint_file,
     str(controller_frequency),
-    str(1.0),
+    str(traj_speed),
+    str(speed_weight),
+    str(descent_rate),
+    str(step_limit),
     str(len(waypoints)),
 ]
 for waypoint in waypoints:
